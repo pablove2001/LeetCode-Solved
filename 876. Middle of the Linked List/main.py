a80e1606427790da4a2dc362ccbbdaa99e2e77c3
Pablo Vergara <vergarapablo2001@gmail.com>
2022-11-06 19:17:03 -0600

    876 2.0

--- 876. Middle of the Linked List/main.py
+++ 876. Middle of the Linked List/main.py
@@ -1,16 +1,11 @@
-
-def main(head):
-    m = head
-    while head.next != None:
-        m = m.next
-        head = head.next
-        if head.next == None:
-            return m
-        head = head.next
-    
-    return m
-
-
-head = []
-
-main(head)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        m = head
+        while head and head.next:
+            m, head = m.next, head.next.next
+        return m
